daf5a7134599c8836835fd2d9c6e8c6298d7a649
enki390 <enki390@gmail.com>
2022-08-09 22:50:15 +0900

    Init project

---
+++ Source/GASLab/Public/Game/GASGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GASGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASLAB_API AGASGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
